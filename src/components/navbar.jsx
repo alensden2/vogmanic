2737962d77254a3dfdb160952c63ad8d7966a20b
alensden2 <alensanjohn1999@gmail.com>
2023-06-20 10:18:02 -0300

    deployment ready

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,11 @@
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import BadgeIcon from "@mui/icons-material/Badge";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import HomeIcon from "@mui/icons-material/Home";
-import Inventory2Icon from "@mui/icons-material/Inventory2";
 import MenuIcon from "@mui/icons-material/Menu";
-import ShowChartIcon from "@mui/icons-material/ShowChart";
 import { AppBar, Box, CssBaseline, IconButton, Toolbar, Typography, useScrollTrigger } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import React from "react";
 import { useNavigate } from "react-router";
 
 const drawerWidth = 240;
-const icons = [
-  <DashboardIcon />,
-  <ShowChartIcon />,
-  <Inventory2Icon />,
-  <AttachMoneyIcon />,
-  <HomeIcon />,
-  <BadgeIcon />,
-];
-const routes = [
-  "/dashboard",
-  "/statistics",
-  "/inventory",
-  "/sales",
-  "/home",
-  "/employees",
-];
+
 
 const AppBarStyled = styled(AppBar, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -45,13 +24,6 @@ const AppBarStyled = styled(AppBar, {
   }),
 }));
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
-}));
 
 const Navbar = ({ isOpen, onToggle }) => {
   const navigate = useNavigate();
