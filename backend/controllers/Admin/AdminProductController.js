cf0b7691aa56fe626b32eb92b155e35c06b73cdd
alensden2 <alensanjohn1999@gmail.com>
2023-07-19 19:29:14 -0300

    products --backend complete

--- backend/controllers/Admin/AdminProductController.js
+++ backend/controllers/Admin/AdminProductController.js
@@ -2,22 +2,74 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const Product = require('../../models/Product')
 // To be replaced with env vars for creds, all hard coded strings will be kept in JSON "resources.json"
 
-const getAllProducts = async (req,res) => {
+/**
+ * Gets all the product available
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+const getAllProducts = async (req, res) => {
     try {
         // finds all the products
         const product = await Product.find();
         res.status(200).json(product);
     } catch (error) {
         res.status(400).json({
-            message : error.message
+            message: error.message
         });
     };
 }
 
-const testAdminProductsController = async (req,res) => {
+const addProduct = async (req, res) => {
+    try {
+        // extract product data
+        const { 
+            name, 
+            description, 
+            price, 
+            shipping_cost,
+            rating,
+            category,
+            image_url
+        } = req.body;
+
+        // create a new product
+        const newProduct = new Product({
+            name,
+            description,
+            description,
+            price,
+            shipping_cost,
+            rating,
+            category,
+            image_url
+        });
+
+        /**
+         * In a real scenerio the image is dumped to a s3 bucket
+         * the reference is stored here 
+         * 
+         * I can also use multer to upload a blob object, but that is not good practice 
+         * to store a base x64 string in the no sql db
+         */
+
+        // save to data 
+        await newProduct.save();
+
+        res.status(200).json({
+            message : "Product saved successfully"
+        })
+    } catch (error) {
+        res.status(400).json({
+            message : error.message,
+        })
+    }
+}
+
+const testAdminProductsController = async (req, res) => {
     res.status(200).json({
-        status : "Works Admin Controller"
+        status: "Works Admin Controller"
     })
 }
 
-module.exports = { getAllProducts, testAdminProductsController}
+module.exports = { getAllProducts, testAdminProductsController, addProduct }
