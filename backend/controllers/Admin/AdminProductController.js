d81a55c8cf5405b248b30a29fc27a13ed86a9336
alensden2 <alensanjohn1999@gmail.com>
2023-07-19 16:22:03 -0300

    fixed routes

--- backend/controllers/Admin/AdminProductController.js
+++ backend/controllers/Admin/AdminProductController.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const Product = require('../../models/Product')
 // To be replaced with env vars for creds, all hard coded strings will be kept in JSON "resources.json"
 const uri = "mongodb+srv://admin:admin%40123@cluster0.htrbjdo.mongodb.net/Cluster0?retryWrites=true&w=majority";
 const dbName = "VogueManiac"
@@ -14,6 +15,22 @@ const client = new MongoClient(uri, {
   });
 
 
-export const getAllProducts = async (req,res) => {
-
+const getAllProducts = async (req,res) => {
+    try {
+        // finds all the products
+        const product = await Product.find();
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json({
+            message : error.message
+        });
+    };
 }
+
+const testAdminProductsController = async (req,res) => {
+    res.status(200).json({
+        status : "Works Admin Controller"
+    })
+}
+
+module.exports = { getAllProducts, testAdminProductsController}
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
+
 const adminProductController = require('../controllers/Admin/AdminProductController')
+// const middleware = require('../middleware/auth')
+
+/** Test Endpoint */
+router.get("/test", adminProductController.testAdminProductsController)
 /** Get all products */
 router.get("/products", adminProductController.getAllProducts) /**Insert Auth MiddleWare */
+
+module.exports = router;
