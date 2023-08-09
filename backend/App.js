936c843a47cc383a116022be3adb31985bd6bf52
Dhruvil Patel <dh828824@dal.ca>
2023-08-09 15:47:06 -0300

    Backend code cleanup and comments for main files

--- backend/App.js
+++ backend/App.js
@@ -3,62 +3,53 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const socketIO = require('socket.io');
-const helmet = require('helmet'); 
+const mongoose = require('mongoose');
+
+// Importing config variables
+const { MONGO_DB_URL, PORT } = require('./config');
+
 const app = express();
+
+// Importing routes
 const userRoutes = require('./routes/userRoutes');
 const productsRoutes = require('./routes/productsRoutes');
 const adminRoutes = require('./routes/adminRoutes.js');
 const orderRoutes = require('./routes/orderRoutes');
 const resaleProductsRoute = require('./routes/resaleProductsRoute');
 const userDashboardRoutes = require('./routes/userDashboardRoutes');
-const mongoose = require('mongoose')
 
-
-app.use(cors()); // Enable CORS for all routes
-app.use(helmet())
+// Middleware for handling CORS
+app.use(cors());
+// Middleware for parsing JSON
 app.use(express.json());
+// Middleware for parsing URL-encoded bodies
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/product', productsRoutes);
-// app.use('/cart_db',productsRoutes);
-
-// app.use('/', authenticationRouter);
-// app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-
-// admin routes
+// Defining routes
 app.use('/admin', adminRoutes);
-
-//user routes
-
 app.use('/users', userRoutes);
-
-app.use('/dashboard', userDashboardRoutes)
-
-// Order Routes
+app.use('/dashboard', userDashboardRoutes);
+app.use('/product', productsRoutes);
 app.use('/order', orderRoutes);
-
-// Resale Product Routes
 app.use('/resale', resaleProductsRoute);
 
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res) {
-    // set locals, only providing error in development
+    // Set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // Send the error response
     res.status(err.status || 500);
     res.send(err.message);
 });
 
-
-// Start the socket server
+// Create HTTP server and set up Socket.IO with CORS options
 const server = http.createServer(app);
 const io = socketIO(server, {
     cors: {
@@ -66,43 +57,41 @@ const io = socketIO(server, {
     }
 });
 
+// Global object to hold client sockets
 global.sockets = {};
 
+// Handling Socket.IO connections
 io.on('connection', socket => {
     console.log('A client connected');
 
     var handshakeData = socket.request;
     const userId = handshakeData._query['userId'];
-    global.sockets[userId] = socket;
-    console.log("middleware:", handshakeData._query['userId']);
+    global.sockets[userId] = socket; // Storing user socket
+    console.log("middleware:", userId);
 
+    // Receiving and echoing messages
     socket.on('message', message => {
         console.log('Received message:', message);
-        socket.emit('message', message);    // Echo the message back to the client
+        socket.emit('message', message);
     });
 
+    // Handling disconnection
     socket.on('disconnect', () => {
         global.sockets[userId] = null;
         console.log('A client disconnected');
     });
 });
 
-// connection made to DB
-
-
-// const serverPort = process.env.PORT || 6001;
-const serverPort = 6001;
-
-mongoose.connect("mongodb+srv://admin:admin%40123@cluster0.htrbjdo.mongodb.net/VogueManiac?retryWrites=true&w=majority", {
+// Connecting to MongoDB and starting the server
+mongoose.connect(MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    server.listen(serverPort, () => {
-        console.log(`Connected to MongoDB, Server listening on port ${serverPort}`);
+    server.listen(PORT, () => {
+        console.log(`Connected to MongoDB, Server listening on port ${PORT}`);
     });
 }).catch((error) => {
-    console.log(`error is ${error}`)
-})
-
+    console.log(`error is ${error}`);
+});
 
 module.exports = app;
---
+++ backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: 6001,
+    MONGO_DB_URL: 'mongodb+srv://admin:admin%40123@cluster0.htrbjdo.mongodb.net/VogueManiac?retryWrites=true&w=majority',
+};
