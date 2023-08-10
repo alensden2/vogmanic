6e45a1eab3752c1c737c469583a56cf6a8657cda
Dhruvil Patel <dh828824@dal.ca>
2023-08-10 13:19:48 -0300

    Comments in backend models defination

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose');
 
+// Defining the schema for a product, containing details like name, description, price, etc.
 const productSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: true, // Name of the product is required
     },
     description: {
         type: String,
-        required: true,
+        required: true, // Description of the product is required
     },
     price: {
         type: Number,
-        required: true,
+        required: true, // Price of the product is required
     },
     shipping_cost: {
         type: Number,
-        required: true,
+        required: true, // Shipping cost for the product is required
     },
     rating: {
         type: Number,
-        required: true,
+        required: true, // Rating of the product is required
     },
     category: {
         type: String,
-        required: true,
+        required: true, // Category of the product is required
     },
     image_url: {
         type: String,
-        required: true,
+        required: true, // URL of the product's image is required
     },
-}, { timestamps: true });
+}, { timestamps: true }); // Including timestamps for created_at and updated_at fields
 
 const Product = mongoose.model('products', productSchema, 'products');
 
-module.exports = Product
+module.exports = Product;
--- backend/models/UserInfo.js
+++ backend/models/UserInfo.js
@@ -1,32 +1,33 @@
 const mongoose = require('mongoose');
 
+// Defining the user information schema
 const userInfoSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    unique: true,
+    ref: 'User',        // Referencing the User model
+    required: true,     // User ID is a required field
+    unique: true,       // Ensuring that the user ID is unique
   },
   username: {
     type: String,
-    required: true,
+    required: true,     // Username is a required field
   },
   bio: {
     type: String,
-    required: false,
+    required: false,    // Bio is an optional field for user profile
   },
   email: {
     type: String,
-    required: true,
+    required: true,     // Email is a required field for communication
   },
   primaryAddress: {
     type: String,
-    required: false,
+    required: false,    // Primary address is optional for shipping
   },
   secondaryAddress: {
-    type: String,
+    type: String,       // Secondary address is optional
   },
-  otherAddresses: [String],
+  otherAddresses: [String],     // Array of other optional addresses
 });
 
 const UserInfo = mongoose.model('UserInfo', userInfoSchema);
